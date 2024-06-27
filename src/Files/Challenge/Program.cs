2cf4cae34b421659ba3b7b6fedb21b43d00bc483
Wisdom Akpasa <127584171+acertainpoggerman@users.noreply.github.com>
2024-06-27 10:32:40 +0100

    Initial Commit (Added Challenge folders)

---
+++ src/Files/Challenge/Program.cs
@@ -0,0 +1,58 @@
+﻿
+// Name:    Wisdom Sampson Akpasa
+// ID:      BU/22C/IT/7153
+
+string resultsFileName = "results.txt";
+string folderName = "FileCollection";
+
+// Dictionary to hold file type information
+Dictionary<string, Dictionary<string, long>> fileTypeTracker = new() {
+    { ".docx", new(){ {"count", 0}, {"size", 0} } } ,
+    { ".pptx", new(){ {"count", 0}, {"size", 0} } } ,
+    { ".xlsx", new(){ {"count", 0}, {"size", 0} } } ,
+};
+
+
+int totalCount = 0;
+long totalSize = 0;
+
+// To Enumerate through all the folders in the specified directory
+DirectoryInfo folder = new(folderName);
+List<string> files = new(Directory.EnumerateFiles(folder.FullName));
+
+foreach (string file in files)
+{
+    FileInfo fileInfo = new(file);
+    if (fileTypeTracker.ContainsKey(fileInfo.Extension)) {
+        fileTypeTracker[fileInfo.Extension]["count"]++;
+        fileTypeTracker[fileInfo.Extension]["size"] += fileInfo.Length;
+        
+        totalCount++;
+        totalSize += fileInfo.Length;
+    }
+}
+
+// Forming the text to put in the "results.txt" file
+const int columnSize = 18;
+// Horizontal Line for cleaner look
+string horizontalRule = new('-', Math.Abs(columnSize) * 3);
+
+// <Start> Creating the Result Text
+string resultText = "\nSUMMARY\n\n";
+resultText += $"{"File Type",-columnSize}{"Count",-columnSize}{"Size",columnSize}\n";
+resultText += $"{horizontalRule}\n";
+foreach (KeyValuePair<string, Dictionary<string, long>> fileType in fileTypeTracker) {
+    resultText += $"{fileType.Key,-columnSize}{$"{fileType.Value["count"]} files",-columnSize}{$"{fileType.Value["size"]:N0} Bytes",columnSize}\n";
+}
+resultText += $"{horizontalRule}\n{"Total",-columnSize}{$"{totalCount} files",-columnSize}{$"{totalSize:N0} Bytes",columnSize}";
+// <End> Creating the Result Text
+
+// Printing to Console
+Console.WriteLine(resultText);
+Console.WriteLine();
+// Writing to the file (results.txt)
+using StreamWriter sw = File.CreateText(resultsFileName);
+sw.WriteLine(resultText);
+sw.Close();
+
+
